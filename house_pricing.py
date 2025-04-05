0769f0c33fc1e0b38cbd7608577b0fceed78221f
ricardont <rontiverosmarquez@gmail.com>
2025-04-04 18:17:27 -0600

    first construction net rate attempt

--- house_pricing.py
+++ house_pricing.py
@@ -14,6 +14,7 @@ import pandas as pd
 import geopandas as gpd
 from shapely.geometry import Point, shape, Polygon
 from shapely import  wkt
+from shapely.wkt import  loads
 import pyproj
 from pyproj import Transformer
 from tqdm import tqdm 
@@ -43,9 +44,6 @@ def scrape_mercadolibre_detail(url):
     lon = 0.000000
     point_projected = Point(0.000000, 0.000000)
     print(str(check_response(sub_response)))
-    print(str(check_response(sub_response)))
-    print(str(check_response(sub_response)))
-    print(str(check_response(sub_response)))
     if check_response(sub_response):
         with open(f"{url[-10:]}_dtl_mlibre_scrape.txt", "w", encoding="utf-8") as file:
             file.write(sub_response.text)
@@ -58,7 +56,8 @@ def scrape_mercadolibre_detail(url):
             map_url = item.select_one(".ui-vip-location").get("src")
         if map_url:
             lat_lon = map_url.get("src").split("center=")[1].split("&zoom")[0].split(",")[0].split("%2C")
-            lon, lat = float(lat_lon[1]),  float(lat_lon[0]) 
+            lat = float(lat_lon[0]) 
+            lon = float(lat_lon[1])  
             transformer = Transformer.from_crs("EPSG:4326", "EPSG:32613", always_xy=True) 
             point_projected = Point(transformer.transform(lon, lat))
     print(f"latitude:{lat}, longitude:{lon}, geometry:{point_projected}")
@@ -234,6 +233,24 @@ def save_to_json(data, filename):
     with open(filename, "w", encoding="utf-8") as f:
         json.dump(data, f, indent=4, ensure_ascii=False)
 
+def shape_zip_to_df(zip_file):
+    zip_file = f"{zip_file}"
+    zipshape = zipfile.ZipFile(open(r'{zip_file}', 'rb'))
+    cpg_file, dbf_file, prj_file, sbn_file, sbx_file, shp_file, xml_file, shx_file = zipshape.namelist()
+    features = shapefile.Reader(
+        shp=zipshape.open(shp_file),
+        shx=zipshape.open(shx_file),
+        dbf=zipshape.open(dbf_file),
+    )  
+    shapes = features.shapes()  # Geometry data
+    records = features.records()  # Attributes
+    # Extract neighborhood names (Replace "NAME_FIELD" with the actual field name)
+    list = []    
+    shapes = features.shapes()  # Geometry data
+    records = features.records()  # Attributes
+    df = gpd.GeoDataFrame(list, geometry='geometry', crs="EPSG:32613")
+    return df
+
 def add_neighborhoods_to_df(locations_df):
     zipshape = zipfile.ZipFile(open(r'.\\data\\080190001_ColoniasyFraccionamientos_2016.zip', 'rb'))
     cpg_file, dbf_file, prj_file, sbn_file, sbx_file, shp_file, xml_file, shx_file = zipshape.namelist()
@@ -255,27 +272,38 @@ def add_neighborhoods_to_df(locations_df):
     # # Convert to GeoDataFrame
     locations = gpd.GeoDataFrame(locations_df, geometry='geometry', crs="EPSG:32613")
     neighborhoods = gpd.GeoDataFrame(neighborhoods_list, geometry='geometry', crs="EPSG:32613")
-    merged_locations = gpd.sjoin(locations, neighborhoods, how="left", predicate="within")
-    merged_neighborhoods = gpd.sjoin(locations, neighborhoods, how="right", predicate="contains")
-    merged_locations = merged_locations[merged_locations.neighborhood.isnull()]
-    merged = pd.concat([merged_neighborhoods, merged_locations], ignore_index=True).drop_duplicates()
-    return merged
+    # Get all locations with and without neighborhoods
+    locations_with_neighborhoods = gpd.sjoin(locations, neighborhoods, how="left", predicate="within")
+    locations_without_neighborhoods = locations_with_neighborhoods[locations_with_neighborhoods['index_right'].isna()].copy()
+    # Get all neighborhoods without locations
+    neighborhoods_with_locations = gpd.sjoin(neighborhoods, locations, how="left", predicate="contains")
+    neighborhoods_without_locations = neighborhoods_with_locations[neighborhoods_with_locations['index_right'].isna()].copy()
+    merged = pd.concat([locations_with_neighborhoods, neighborhoods_without_locations], ignore_index=True)
+    return locations_with_neighborhoods
 
-def retry_get_location_from_existing_df(df):
-    df_without_location = df[df['latitude'] + df['longitude'] == 0.000000]
-    print("Scraping again..." + str(len(df_without_location)) + " records")
+def scrap_location_from_df(df):
+    print("Scraping again..." + str(len(df)) + " records")
+    affected_records = 0
+    df_new = df.copy()
     for index, row in tqdm(df.iterrows(), total=df.shape[0]):
-        if row["latitude"] + row["longitude"] == 0 :  # Only scrape if non neighborhood
-            location = scrape_mercadolibre_detail(row["link"])
-            if location["latitude"] + location["longitude"] > 0.000000:
-                # scrapping again succesfully 
-                print(location["latitude"])
-                print(location["longitude"])
-                df.at[index, "latitude"] = location["latitude"]
-                df.at[index, "longitude"] = location["latitude"]
-# print(features)
+        location = scrape_mercadolibre_detail(row["link"])
+        if location["latitude"] + location["longitude"] != 0.000000:
+            # scrapping again succesfully 
+            print(location["latitude"])
+            print(location["longitude"])
+            print(location["geometry"])
+            df.at[index, "latitude"] = location["latitude"]
+            df.at[index, "longitude"] = location["longitude"]
+            df.at[index, "geometry"] = location["geometry"]
+            affected_records += 1
+        print("Scraped " + str(affected_records) + " records")        
+    return df
 def df_snippets(type="shapefile_convert", df=None, geometry_field="geometry", request_response="", text_response_file=""):
     output = None
+    # df_new['geometry'] = df_new['geometry'].apply(wkt.loads)
+    # gdf['geometry'] = gdf['geometry'].apply(loads)
+    # df_new[df_new["geometry"] == Point(0, 0)]
+    # df_without_location = df[ df.geometry == loads('POINT (0 0)') ]
     if type == "shapefile_convert":
         gdf = gpd.GeoDataFrame(df, geometry='geometry')
         gdf.to_file("output.shp", driver="ESRI Shapefile")
@@ -306,6 +334,26 @@ def df_snippets(type="shapefile_convert", df=None, geometry_field="geometry", re
     else:
         print("No snippet found")    
     return output
+def shapezip_to_df(zip_file):
+    zipshape = zipfile.ZipFile(open(zip_file, 'rb'))
+    cpg_file, dbf_file, prj_file, sbn_file, sbx_file, shp_file, xml_file, shx_file = zipshape.namelist()
+    features = shapefile.Reader(
+        shp=zipshape.open(shp_file),
+        shx=zipshape.open(shx_file),
+        dbf=zipshape.open(dbf_file),
+    )  
+    fields = [field[0] for field in features.fields[1:]]
+    shapes = features.shapes()  # Geometry data
+    records = features.records()  # Attributes
+    list = []
+    for sr, geom in zip(records, shapes):
+        row = dict(zip(fields, sr))
+        row["geometry"] = shape(geom)
+        list.append(row)
+    # # Convert to GeoDataFrame   
+    df = gpd.GeoDataFrame(list, geometry='geometry', crs="EPSG:32613")
+    return df
+
 # Main script
 if __name__ == "__main__":
     df = scrap_by_city()
